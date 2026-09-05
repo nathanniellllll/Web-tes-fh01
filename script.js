dc6ef2f7b66f9cd8bc63d14b0f8dd02c44ee7b7c
nathanniellllll <sandronathaniel08@gmail.com>
2026-09-05 22:26:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+/* JavaScript / fungsi interaktif. Edit hanya jika ingin mengubah fungsi website. */
+
+document.querySelectorAll('a[href]').forEach(a=>a.addEventListener('click',()=>document.body.classList.add('leaving')));
